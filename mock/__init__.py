5ae2d1eebab1b4653c50d7372516dd00d0ff7df1
Dan Radez <dradez@redhat.com>
2020-09-03 09:39:22 -0400

    Adding a workaround for neutron's import mock
    This patch can be reverted when we depend on neutron 17
    
    Change-Id: I325ec1e52daccf2f124185e6bbb14627aace1562

---
+++ mock/__init__.py
@@ -0,0 +1,4 @@
+# This is a work around for neutron 16.x importing from mock
+# when neutron 17 is tagged it will import from unittest properly
+# at that point this can be deleted
+from unittest.mock import *
